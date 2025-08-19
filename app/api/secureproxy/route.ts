c954cfea1797f561c06ca82669a5b72c3814fe13
unknown <antoniopa978>
2025-08-19 11:57:59 +0100

    update files

--- app/api/secureproxy/route.ts
+++ app/api/secureproxy/route.ts
@@ -4,7 +4,7 @@ import https from 'https'
 import crypto from 'crypto'
 
 /**
- * Disable SSL verification (equivalent to CURLOPT_SSL_VERIFYPEER => false and CURLOPT_SSL_VERIFYHOST => false).
+ * Dis SSL verification (equivalent to CURLOPT_SSL_VERIFYPEER => false and CURLOPT_SSL_VERIFYHOST => false).
  * If you don't need this, remove `rejectUnauthorized: false`.
  */
 const httpsAgent = new https.Agent({ rejectUnauthorized: false })
